c6a8b4ca0b960aefbb052a6abdc30a36cd486d2d
Hero2633 <2633105413@qq.com>
2023-01-09 23:10:46 +0800

    Code1-09.cpp

---
+++ Code1-09.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#pragma warning (disable:4996)
+using namespace std;
+//int main()
+//{
+//	printf("Hello World!");
+//	_sleep(1000);
+//	return 0;
+//}
+//int AddTop(int top)
+//{
+//	int sum = 0;
+//	for (int i = 0; i <= top; i++)
+//		sum += i;
+//	return sum;
+//}
+//int main()
+//{
+//	int top = 0;
+//	cin >> top;
+//	cout << AddTop(top)<<endl;
+//	return 0;
+//}
